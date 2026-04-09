5d9dc1b3574c2499aaf575ef1e512772024a2b01
Aaron <247822488+aarontam-tech@users.noreply.github.com>
2026-04-08 22:03:08 -0400

    Add the return value
    The function must return certain integers to pass the test.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+  if((Number.isInteger(int1) && int1 >= 0) && (Number.isInteger(int2) && int2 >= 0)) {
+    let lesserInt = Math.min(int1, int2),
+      greaterInt = Math.max(int1, int2),
+      sum = greaterInt * (greaterInt + 1) / 2 - (lesserInt - 1) * lesserInt / 2;
+    return sum;
+  }
+  return 'ERROR';
 };
 
 // Do not edit below this line
