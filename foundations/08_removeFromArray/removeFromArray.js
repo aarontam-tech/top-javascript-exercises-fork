5fc841a1da4ecc7f150a4fdf8c5b3018fb96d1f6
Aaron <247822488+aarontam-tech@users.noreply.github.com>
2026-04-08 21:27:15 -0400

    Add the return value
    The function must return certain arrays to pass the test.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removalTargets) {
+  let newArray = [];
+  let keep;
+  for(let i = 0; i < array.length; i++) {
+    keep = true;
+    for(let j = 0; j < removalTargets.length && keep; j++) {
+      if(array[i] === removalTargets[j]) {
+        keep = false;
+      }
+    }
+    if(keep) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
