2d4f19cc782b795830273e554fcfddbb31f8edbc
Aaron <247822488+aarontam-tech@users.noreply.github.com>
2026-04-20 16:35:14 -0400

    Add the return value to the findTheOldest function
    The function must return a certain object to pass the test.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((pre, cur) =>
+    pre && (pre.yearOfDeath ? pre.yearOfDeath - pre.yearOfBirth: new Date().getFullYear() - pre.yearOfBirth) >
+    (cur.yearOfDeath ? cur.yearOfDeath - cur.yearOfBirth: new Date().getFullYear() - cur.yearOfBirth) ? pre : cur);
 };
 
 // Do not edit below this line
