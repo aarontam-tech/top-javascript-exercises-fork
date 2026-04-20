7cf75fedcfdbcc25a227726c29555e824ed1de4e
Aaron <247822488+aarontam-tech@users.noreply.github.com>
2026-04-20 15:56:36 -0400

    Add the return value to the fibonacci function
    The function must return certain integers to pass the test.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(sequenceNumber) {
+  if(Number(sequenceNumber) === 0) {
+    return 0;
+  } else if(Number(sequenceNumber) < 0) {
+    return "OOPS"
+  }
+  let previous = 0;
+  let current = 1;
+  for(let i = 1; i < Number(sequenceNumber); i++) {
+    let next = current + previous;
+    previous = current;
+    current = next;
+  }
+  return current;
 };
 
 // Do not edit below this line
