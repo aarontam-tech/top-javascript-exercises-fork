a8823a78dffed903ebbee125a12b529982b83c2f
Aaron <247822488+aarontam-tech@users.noreply.github.com>
2026-04-20 15:09:52 -0400

    Add the return value to the palindromes function
    The function must return certain booleans to pass the test.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let charArray = [...string.toLowerCase()].filter((character) => character.match("[a-z0-9]"));
+  for(let i = 0; i < charArray.length/2; i++) {
+    if(charArray[i] != charArray[charArray.length-1-i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
